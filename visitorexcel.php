34d5f7ea63aa2b598a62c317aa3894600ddcd473
rosmark <rosmark@users.noreply.github.com>
2016-08-19 14:18:02 +0200

    Update visitorexcel.php

--- visitorexcel.php
+++ visitorexcel.php
@@ -2,7 +2,7 @@
 
 include 'PHPexcel/Classes/PHPExcel.php';
 include 'PHPexcel/Classes/PHPExcel/IOFactory.php';
-$pdo = new PDO('mysql:dbname=u597716772_1;host=mysql.hostinger.se', 'u597716772_1', 'Daniel5%');                                                            
+$pdo = new PDO('mysql:dbname=u597716772_1;host=31.220.20.207', 'u597716772_1', 'Daniel5%');                                                            
 $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );  
 
 // Create new PHPExcel object
