5fc7263c2b4bd00c3f5c4f4a3b55bc0387adc970
rosmark <rosmark@users.noreply.github.com>
2016-08-21 16:28:28 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:dbname=u597716772_1;host=mysql.hostinger.se', 'u597716772_1', 'Daniel5%');                                                            
+$pdo = new PDO('mysql:dbname=u597716772_1;host=31.220.20.207', 'u597716772_1', 'Daniel5%');                                                            
 $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);   //,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 	
 ?>
